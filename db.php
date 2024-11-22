8d23de74edf11eaf9189bf5a388f5dd43460560d
xCamilo10 <bryant.xcamilo10@gmail.com>
2024-11-22 15:13:08 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=libreria'; // Cambia 'libreria' si es diferente el nombre
+$dsn = 'mysql:host=localhost;dbname=db_libreria'; // Cambia 'libreria' si es diferente el nombre
 $username = 'root'; // Cambia esto por tu usuario
-$password = ''; // Cambia esto por tu contraseña si la tienes configurada
+$password = 'root'; // Cambia esto por tu contraseña si la tienes configurada
 
 try {
     $pdo = new PDO($dsn, $username, $password);
